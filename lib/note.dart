5c99621c349e5abef4627c3ccdceaf17683e5fb8
Abolela-H45 <omarabolela@icloud.com>
2025-08-27 22:18:23 +0800

    Note Model

---
+++ lib/note.dart
@@ -0,0 +1,54 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+class Note {
+  final String id;
+  final String title;
+  final String description;
+
+  const Note({
+    this.id = '',
+    required this.title,
+    required this.description,
+  });
+
+  factory Note.fromMap(Map<String, dynamic> m, {String id = ''}) {
+    return Note(
+      id: id,
+      title: (m['title'] ?? '').toString(),
+      description: (m['description'] ?? '').toString(),
+    );
+  }
+
+  factory Note.fromDoc(DocumentSnapshot<Map<String, dynamic>> doc) {
+    final m = doc.data() ?? <String, dynamic>{};
+    return Note(
+      id: doc.id,
+      title: (m['title'] ?? '').toString(),
+      description: (m['description'] ?? m['content'] ?? '').toString(),
+    );
+  }
+
+  Map<String, dynamic> toMap() => {
+        'title': title,
+        'description': description,
+      };
+}
+
+class NotesBundle {
+  final String uid;
+  final List<Note> notes;
+
+  const NotesBundle({required this.uid, required this.notes});
+
+  factory NotesBundle.fromDoc(DocumentSnapshot<Map<String, dynamic>> doc) {
+    final data = doc.data() ?? <String, dynamic>{};
+    final raw = (data['documents'] as List<dynamic>? ?? []);
+    final items = <Note>[];
+    for (final item in raw) {
+      if (item is Map<String, dynamic>) {
+        items.add(Note.fromMap(item));
+      }
+    }
+    return NotesBundle(uid: doc.id, notes: items);
+  }
+}
