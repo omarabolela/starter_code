424c1fc17c488a381deb94da6284f50cbebb2b43
Abolela-H45 <omarabolela@icloud.com>
2025-08-27 20:41:08 +0800

    Part A: Setting Up Firebase and Flutter

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,16 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:map_exam/firebase_options.dart';
 
 // import 'login_screen.dart';
 import 'home_screen.dart';
 // import 'edit_screen.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const App());
 }
 
